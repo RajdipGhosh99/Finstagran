0d5739f063909fa5746b001c5a986d649906537f
Suman <sumannaskar321@gmail.com>
2023-04-09 16:09:17 +0530

    handler added

--- Server/Middleware/errorHandler.js
+++ Server/Middleware/errorHandler.js
@@ -1,8 +1,10 @@
 function errorHandler(error, req, res, next) {
 	if (res) {
 		return res.status(error.statusCode || 500).json({
+			status: "error",
 			message: error.message || "Something went wrong!",
 			error: error.name || "Server Error",
+			data: null,
 		});
 	}
 	// console.log("errorHandler", error && error.stack ? error.stack : error);
--- Server/Utils/tryCatch.js
+++ Server/Utils/tryCatch.js
@@ -1,6 +1,7 @@
 exports.tryCatch = (controller) => async (req, res, next) => {
 	try {
 		await controller(req, res);
+		// return next(res);
 	} catch (error) {
 		console.log("tryCatch", error);
 		return next(error);
