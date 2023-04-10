faeb2154622b1e7892d494e54439fa99025179a4
Suman <sumannaskar321@gmail.com>
2023-04-10 07:09:30 +0530

    2 api added

--- Server/Models/userposts.model.js
+++ Server/Models/userposts.model.js
@@ -1,29 +1,32 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const userpostSchema = new mongoose.Schema({
-    user_id: {
-        trim: true,
-        required: true,
-        type: mongoose.Schema.ObjectId,
-    },
-    title: {
-        trim: true,
-        required: true,
-        type: String
-    },
-    contents: [],
-    tags: [],
-    time_stamp: {
-        required: true,
-        type: Date,
-        default: new Date()
-    },
-    is_deleted: {
-        type: Boolean,
-        default: false
-    }
+const userpostSchema = new mongoose.Schema(
+	{
+		user_id: {
+			trim: true,
+			required: true,
+			type: ObjectId,
+		},
+		title: {
+			trim: true,
+			required: true,
+			type: String,
+		},
+		contents: [],
+		tags: [],
+		time_stamp: {
+			required: true,
+			type: Date,
+			default: new Date(),
+		},
+		is_deleted: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{ collection: "userposts_master" },
+);
 
-}, { collection: 'userposts_master' })
-
-const PostsModel = mongoose.model('posts', userpostSchema)
-module.exports = PostsModel
+const PostsModel = mongoose.model("posts", userpostSchema);
+module.exports = PostsModel;
--- Server/Routes/Posts/index.js
+++ Server/Routes/Posts/index.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const router = express.Router()
-const postsController = require('./posts.controller')
+const express = require("express");
+const router = express.Router();
+const postsController = require("./posts.controller");
+const postsActivityController = require("./posts_activity.controller");
+const { tryCatch } = require("../../Utils/tryCatch");
 
-router.post('/create', postsController.createPost)
+router.post("/create", postsController.createPost);
+router.post(
+	"/like_or_dislike",
+	tryCatch(postsActivityController.add_like_dislike),
+);
+router.post(
+	"/user_list_for_likes/:pc_id",
+	tryCatch(postsActivityController.getUsersLikedPostOrComment),
+);
 
-module.exports = router
+module.exports = router;
